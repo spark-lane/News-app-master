8c107bea9836a5571c3fd868b265442f92a13f4a
spark-lane <omsore47@gmail.com>
2023-06-05 15:16:22 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+
+import './App.css';
+
+import React, { useState } from 'react'
+import Navbar from './Components/Navbar';
+import News from './Components/News';
+import { BrowserRouter as Router, Route,Routes} from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar'
+
+const App=()=> {
+ const  pageSize =5;
+  const [progress, setProgress] = useState(0)
+  
+ 
+
+    return (
+      <div>
+        <Router>
+        <Navbar/>
+        <LoadingBar
+        hight={3}
+        color='#f11946'
+        progress={progress}
+      />
+        <Routes>
+          <Route exact path="/"element={<News setProgress={setProgress} key="general" pageSize={pageSize}  country="in" category="general" />}/>
+          <Route exact path="/business"element={<News setProgress={setProgress} key="business" pageSize={pageSize}  country="in" category="business" />}/>
+          <Route exact path="/entertainment"element={<News setProgress={setProgress} key="entertainment" pageSize={pageSize}  country="in" category="entertainment" />}/>
+          <Route exact path="/health"element={<News setProgress={setProgress} key="health" pageSize={pageSize}  country="in" category="health" />}/>
+          <Route exact path="/science"element={<News setProgress={setProgress} key="science" pageSize={pageSize}  country="in" category="science" />}/>
+          <Route exact path="/sports"element={<News setProgress={setProgress} key="sports" pageSize={pageSize}  country="in" category="sports" />}/>
+          <Route exact path="/technology"element={<News setProgress={setProgress} key="technology" pageSize={pageSize}  country="in" category="technology" />}/>
+          </Routes>
+        </Router>
+      </div>
+    )
+  }
+export default App;
